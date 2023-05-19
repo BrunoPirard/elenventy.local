199e007857cfbf0e48db4490f393d56ad5571ecd
BrunoPirard <bruno.pirard@bulgaweb.com>
2023-05-19 10:37:04 +0300

    18 mai 10:36

--- .eleventy.js
+++ .eleventy.js
@@ -5,12 +5,22 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const fs = require("fs");
+const NOT_FOUND_PATH = "_site/404.html";
 
 module.exports = function (eleventyConfig) {
 
+    // Plugins
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addPlugin(syntaxHighlight);
+    eleventyConfig.addPlugin(pluginRss, {
+    posthtmlRenderOptions: {
+      closingSingleTag: "default" // opt-out of <img/>-style XHTML single tags
+    }
+    });
+
     // PassTrough
-    eleventyConfig.addPassthroughCopy('robots.txt')
+    eleventyConfig.addPassthroughCopy('./src/robots.txt');
+    eleventyConfig.addPassthroughCopy('./src/.htaccess');
     eleventyConfig.addPassthroughCopy("./src/assets/fonts");
     eleventyConfig.addPassthroughCopy("./src/assets/images");
     eleventyConfig.addPassthroughCopy("./src/assets/js");
@@ -32,22 +42,43 @@ module.exports = function (eleventyConfig) {
     });
 
     // Override Browsersync defaults (used only with --serve)
-    eleventyConfig.setBrowserSyncConfig({
-      callbacks: {
-        ready: function(err, browserSync) {
-          const content_404 = fs.readFileSync('_site/404.html');
+    // eleventyConfig.setBrowserSyncConfig({
+    //   callbacks: {
+    //     ready: function(err, browserSync) {
+    //       const content_404 = fs.readFileSync('_site/404.html');
 
-          browserSync.addMiddleware("*", (req, res) => {
-            // Provides the 404 content without redirect.
-            res.writeHead(404, {"Content-Type": "text/html; charset=UTF-8"});
-            res.write(content_404);
-            res.end();
-          });
-        },
-      },
-      ui: false,
-      ghostMode: false
-    });
+    //       browserSync.addMiddleware("*", (req, res) => {
+    //         // Provides the 404 content without redirect.
+    //         res.writeHead(404, {"Content-Type": "text/html; charset=UTF-8"});
+    //         res.write(content_404);
+    //         res.end();
+    //       });
+    //     },
+    //   },
+    //   ui: false,
+    //   ghostMode: false
+    // });
+
+    // Eleventy code for 404 page
+    eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          if (!fs.existsSync(NOT_FOUND_PATH)) {
+            throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
+          }
+
+          const content_404 = fs.readFileSync(NOT_FOUND_PATH);
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
 
     /* Markdown plugins */
     // https://www.11ty.dev/docs/languages/markdown/
